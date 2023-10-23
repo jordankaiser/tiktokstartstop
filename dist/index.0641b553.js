295973c29e121ddb658cecc12b0e9fe9342096d8
Jordan Kaiser <jordan.kaiser@thinkshout.com>
2023-10-23 16:36:07 -0500

    basic functionality in place

--- dist/index.0641b553.js
+++ dist/index.0641b553.js
@@ -575,8 +575,12 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"bNKaB":[function(require,module,exports) {
-const tiktokWrapper = document.querySelector(".tiktok-wrapper");
+const tiktokWrappers = document.querySelectorAll(".tiktok-wrapper");
 const embedCode = '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@traveloregon/video/7252078322774002986" data-video-id="7252078322774002986" data-embed-from="oembed" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@traveloregon" href="https://www.tiktok.com/@traveloregon?refer=embed">@traveloregon</a> <p>Stay cool, PDX \uD83C\uDF67 <a title="icecream" target="_blank" href="https://www.tiktok.com/tag/icecream?refer=embed">#icecream</a> <a title="portland" target="_blank" href="https://www.tiktok.com/tag/portland?refer=embed">#portland</a> <a title="portlandicecream" target="_blank" href="https://www.tiktok.com/tag/portlandicecream?refer=embed">#portlandicecream</a> </p> <a target="_blank" title="\u266C cupid twin version - \u27E1 TRACYTRACKS \u27E1" href="https://www.tiktok.com/music/cupid-twin-version-7204756346426755886?refer=embed">\u266C cupid twin version - \u27E1 TRACYTRACKS \u27E1</a> </section> </blockquote>';
+const embedCodes = [
+    '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@traveloregon/video/7252078322774002986" data-video-id="7252078322774002986" data-embed-from="oembed" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@traveloregon" href="https://www.tiktok.com/@traveloregon?refer=embed">@traveloregon</a> <p>Stay cool, PDX \uD83C\uDF67 <a title="icecream" target="_blank" href="https://www.tiktok.com/tag/icecream?refer=embed">#icecream</a> <a title="portland" target="_blank" href="https://www.tiktok.com/tag/portland?refer=embed">#portland</a> <a title="portlandicecream" target="_blank" href="https://www.tiktok.com/tag/portlandicecream?refer=embed">#portlandicecream</a> </p> <a target="_blank" title="\u266C cupid twin version - \u27E1 TRACYTRACKS \u27E1" href="https://www.tiktok.com/music/cupid-twin-version-7204756346426755886?refer=embed">\u266C cupid twin version - \u27E1 TRACYTRACKS \u27E1</a> </section> </blockquote>',
+    '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@scout2015/video/6718335390845095173" data-video-id="6718335390845095173" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@scout2015" href="https://www.tiktok.com/@scout2015?refer=embed">@scout2015</a> Scramble up ur name &#38; I\u2019ll try to guess it\uD83D\uDE0D\u2764\uFE0F <a title="foryoupage" target="_blank" href="https://www.tiktok.com/tag/foryoupage?refer=embed">#foryoupage</a> <a title="petsoftiktok" target="_blank" href="https://www.tiktok.com/tag/petsoftiktok?refer=embed">#petsoftiktok</a> <a title="aesthetic" target="_blank" href="https://www.tiktok.com/tag/aesthetic?refer=embed">#aesthetic</a> <a target="_blank" title="\u266C original sound - tiff" href="https://www.tiktok.com/music/original-sound-6689804660171082501?refer=embed">\u266C original sound - tiff</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>'
+];
 const start = document.querySelector(".start");
 const stop = document.querySelector(".stop");
 function test1() {
@@ -591,15 +595,19 @@ function test1() {
 }
 test1();
 function removeTikTok() {
-    const tiktokEmbed = document.querySelector(".tiktok-embed");
-    tiktokEmbed.remove();
+    const tiktokEmbeds = document.querySelectorAll(".tiktok-embed");
+    tiktokEmbeds.forEach(function(tiktokEmbed) {
+        tiktokEmbed.remove();
+    });
     const tiktokScripts = document.querySelectorAll('script[src*="tiktok"]');
     tiktokScripts.forEach(function(script) {
         script.remove();
     });
 }
 function addTikTok() {
-    tiktokWrapper.innerHTML = embedCode;
+    embedCodes.forEach((embedCode, key)=>{
+        tiktokWrappers[key].innerHTML = embedCode;
+    });
     addScript();
 }
 function addScript() {
--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
-const tiktokWrapper = document.querySelector('.tiktok-wrapper');
+const tiktokWrappers = document.querySelectorAll('.tiktok-wrapper');
 const embedCode = '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@traveloregon/video/7252078322774002986" data-video-id="7252078322774002986" data-embed-from="oembed" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@traveloregon" href="https://www.tiktok.com/@traveloregon?refer=embed">@traveloregon</a> <p>Stay cool, PDX 🍧 <a title="icecream" target="_blank" href="https://www.tiktok.com/tag/icecream?refer=embed">#icecream</a> <a title="portland" target="_blank" href="https://www.tiktok.com/tag/portland?refer=embed">#portland</a> <a title="portlandicecream" target="_blank" href="https://www.tiktok.com/tag/portlandicecream?refer=embed">#portlandicecream</a> </p> <a target="_blank" title="♬ cupid twin version - ⟡ TRACYTRACKS ⟡" href="https://www.tiktok.com/music/cupid-twin-version-7204756346426755886?refer=embed">♬ cupid twin version - ⟡ TRACYTRACKS ⟡</a> </section> </blockquote>'
+const embedCodes = [
+  '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@traveloregon/video/7252078322774002986" data-video-id="7252078322774002986" data-embed-from="oembed" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@traveloregon" href="https://www.tiktok.com/@traveloregon?refer=embed">@traveloregon</a> <p>Stay cool, PDX 🍧 <a title="icecream" target="_blank" href="https://www.tiktok.com/tag/icecream?refer=embed">#icecream</a> <a title="portland" target="_blank" href="https://www.tiktok.com/tag/portland?refer=embed">#portland</a> <a title="portlandicecream" target="_blank" href="https://www.tiktok.com/tag/portlandicecream?refer=embed">#portlandicecream</a> </p> <a target="_blank" title="♬ cupid twin version - ⟡ TRACYTRACKS ⟡" href="https://www.tiktok.com/music/cupid-twin-version-7204756346426755886?refer=embed">♬ cupid twin version - ⟡ TRACYTRACKS ⟡</a> </section> </blockquote>',
+  '<blockquote class="tiktok-embed" cite="https://www.tiktok.com/@scout2015/video/6718335390845095173" data-video-id="6718335390845095173" style="max-width: 605px;min-width: 325px;" > <section> <a target="_blank" title="@scout2015" href="https://www.tiktok.com/@scout2015?refer=embed">@scout2015</a> Scramble up ur name &#38; I’ll try to guess it😍❤️ <a title="foryoupage" target="_blank" href="https://www.tiktok.com/tag/foryoupage?refer=embed">#foryoupage</a> <a title="petsoftiktok" target="_blank" href="https://www.tiktok.com/tag/petsoftiktok?refer=embed">#petsoftiktok</a> <a title="aesthetic" target="_blank" href="https://www.tiktok.com/tag/aesthetic?refer=embed">#aesthetic</a> <a target="_blank" title="♬ original sound - tiff" href="https://www.tiktok.com/music/original-sound-6689804660171082501?refer=embed">♬ original sound - tiff</a> </section> </blockquote> <script async src="https://www.tiktok.com/embed.js"></script>',  
+];
 const start = document.querySelector('.start');
 const stop = document.querySelector('.stop');
 
@@ -17,8 +21,10 @@ function test1() {
 test1();
 
 function removeTikTok() {
-  const tiktokEmbed = document.querySelector('.tiktok-embed');
-  tiktokEmbed.remove();
+  const tiktokEmbeds = document.querySelectorAll('.tiktok-embed');
+  tiktokEmbeds.forEach(function(tiktokEmbed) {
+    tiktokEmbed.remove();
+  });
   
   const tiktokScripts = document.querySelectorAll('script[src*="tiktok"]');
   tiktokScripts.forEach(function(script) {
@@ -27,7 +33,9 @@ function removeTikTok() {
 }
 
 function addTikTok() {
-  tiktokWrapper.innerHTML = embedCode;
+  embedCodes.forEach((embedCode, key) => {
+    tiktokWrappers[key].innerHTML = embedCode;
+  });
   addScript();
 }
 
